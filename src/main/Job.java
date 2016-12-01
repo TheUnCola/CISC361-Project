9adc7cc057e891dcee5c2857ac15321f1a4d043c
Steven <steven.sell1@gmail.com>
2016-11-30 21:16:07 -0500

    Job

--- src/main/Job.java
+++ src/main/Job.java
@@ -2,15 +2,58 @@ package main;
 
 public class Job {
 
-	int priority;
-	int timeArrive;
-	int process;
-	int jobNum;
-	int mem;
-	int rT;
+	private int priority;
+	private int timeArrive;
+	private int process;
+	private int jobNum;
+	private int mem;
+	private int rT;
+	private int numDev;
 	
-	public Job() {
-		
+	public Job(int priority, int timeArrive, int process, int jobNum, int mem, int rT, int numDev) {
+		this.priority = priority;
+		this.timeArrive = timeArrive;
+		this.process = process;
+		this.jobNum = jobNum;
+		this.mem = mem;
+		this.rT = rT;
+		this.numDev = numDev;
+	}
+
+	public int getProcess() {
+		return process;
+	}
+
+	public void setProcess(int process) {
+		this.process = process;
+	}
+
+	public int getNumDev() {
+		return numDev;
+	}
+
+	public void setNumDev(int numDev) {
+		this.numDev = numDev;
+	}
+
+	public int getPriority() {
+		return priority;
+	}
+
+	public int getTimeArrive() {
+		return timeArrive;
+	}
+
+	public int getJobNum() {
+		return jobNum;
+	}
+
+	public int getMem() {
+		return mem;
+	}
+
+	public int getrT() {
+		return rT;
 	}
 
 }
